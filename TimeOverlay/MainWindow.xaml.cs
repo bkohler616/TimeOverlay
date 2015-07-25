3233cdea41b074b369323d4e1e29cfe786821c99
riku12124 <riku12124@hotmail.com>
2015-07-25 15:12:58 -0400

    Create Window

---
+++ TimeOverlay/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimeOverlay
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow: Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object o, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if(e.ChangedButton == MouseButton.Left)
+				this.DragMove();
+		}
+
+		private void Window_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
