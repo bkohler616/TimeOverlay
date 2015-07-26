4a3af08659f6f164c1f19ccdb302ea4f2dc889b3
riku12124 <riku12124@hotmail.com>
2015-07-26 14:49:04 -0400

    Added serilizable settings to xml output.
    Added an additions.txt to note what I have to do for this project, and maybe more.
    
    Added a regex to check the hex string (might be moved to settingswindow)

--- TimeOverlay/MainWindow.xaml.cs
+++ TimeOverlay/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.ComponentModel;
+using System.IO;
 using System.Timers;
 using System.Windows;
 using System.Windows.Input;
+using System.Xml.Serialization;
 
 namespace TimeOverlay
 {
@@ -12,6 +15,9 @@ namespace TimeOverlay
 	{
 		private readonly Timer _updateTime;
 		private DateTime _currentDateTime;
+		private SettingsWindow settingsWindowAccess;
+		private static string _path;
+
 		public MainWindow()
 		{
 
@@ -21,18 +27,28 @@ namespace TimeOverlay
 			_updateTime.Elapsed += UpdateTime_Elapsed;
 			_updateTime.Start();
 
+			_path = (new FileInfo(System.Reflection.Assembly.GetEntryAssembly().Location)).Directory + "//TimeOverlaySettings.xml";
 
+			//If settings save exists, deserialize it to SettingsWindow
+			if(File.Exists(_path))
+			{
+				XmlSerializer reader = new XmlSerializer(typeof(SettingsInfo));
+				StreamReader file = new StreamReader(_path);
+				settingsWindowAccess = new SettingsWindow((SettingsInfo)reader.Deserialize(file));
+			}
+			settingsWindowAccess = new SettingsWindow();
+			settingsWindowAccess.Hide();
 		}
 
 		private void UpdateTime_Elapsed(object sender, ElapsedEventArgs elapsedEventArgs)
 		{
 			_updateTime.Stop();
 			_currentDateTime = DateTime.Now;
+			//Invoke another thread to input content
 			Application.Current.Dispatcher.BeginInvoke((Action) delegate()
 			{
-				LblDate.Content = _currentDateTime.DayOfWeek + ", " + _currentDateTime.Day + "/" + _currentDateTime.Month + "/" +
-				                  _currentDateTime.Year;
-				LblTime.Content = _currentDateTime.Hour + ":" + _currentDateTime.Minute + "." + _currentDateTime.Second;
+				LblDate.Content = _currentDateTime.DayOfWeek + ", " + _currentDateTime.Month + "/" + _currentDateTime.Day + "/" + _currentDateTime.Year;
+				LblTime.Content = _currentDateTime.Hour%12 + ":" + _currentDateTime.Minute + "." + _currentDateTime.Second;
 			});
 			_updateTime.Start();
 		}
@@ -61,6 +77,39 @@ namespace TimeOverlay
 			LblTime.Content = "Getting Time...";
 		}
 
+		private void ShowSettings_Click(object sender, RoutedEventArgs e)
+		{
+			settingsWindowAccess.Show();
+		}
+
 		//TODO: Add context menu items to change font and time/date styles
+		private void ShowAbout_Click(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		//TODO: Send and recieve settings info to and from the settings window to save into XML Serilizable
+		private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+		{
+			MessageBox.Show("Saving settings...");
+			SettingsInfo saveSettings = settingsWindowAccess.Settings;
+			XmlSerializer writer = new XmlSerializer(typeof(SettingsInfo));
+			FileStream file = File.Create(_path);
+
+			try
+			{
+				writer.Serialize(file, saveSettings);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error writing settings to document:" + ex.Message + "..... " + ex.StackTrace);
+			}
+			finally
+			{
+				file.Close();
+			}
+			
+			
+		}
 	}
 }
---
+++ TimeOverlay/SettingsInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Navigation;
+
+namespace TimeOverlay
+{
+	public class SettingsInfo
+	{
+		private string _timeTextColor;
+		private string _dateTextColor;
+
+		public SettingsInfo()
+		{
+			_timeTextColor = "ffffff";
+			_dateTextColor = "808080";
+		}
+		public string TimeTextColor
+		{
+			get { return _timeTextColor; }
+			set { _timeTextColor = CheckHex(value) ? value : "ffffff"; }
+		}
+
+		public string DateTextColor
+		{
+			get { return _dateTextColor; }
+			set { _dateTextColor = CheckHex(value) ? value : "808080"; }
+		}
+
+		private bool CheckHex(String hexString)
+		{
+			Regex rgx = new Regex(@"/([A-Z]|[0-9])/g");
+			return rgx.IsMatch(hexString);
+		}
+	}
+}
